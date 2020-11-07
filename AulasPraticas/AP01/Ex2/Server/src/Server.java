fc7b262de1d0b396201827404e10674b54873f26
Guilherme Arede <gfsarede@gmail.com>
2020-11-07 15:02:59 +0000

    implemented server

--- AulasPraticas/AP01/Ex2/Server/src/Server.java
+++ AulasPraticas/AP01/Ex2/Server/src/Server.java
@@ -2,6 +2,9 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
 public class Server implements ILeiloes{
@@ -9,6 +12,8 @@ public class Server implements ILeiloes{
     private static final String serverIP = "localhost";
     private static final int registerPort = 7000;
     private static final int svcPort = 7001;
+
+    private static HashMap<String, Pair> leiloes;
 
     public static void main(String[] args) {
 
@@ -22,6 +27,8 @@ public class Server implements ILeiloes{
             Registry registry = LocateRegistry.createRegistry(registerPort);
             registry.rebind("RemServer", stubSvc);
 
+            leiloes = new HashMap<>();
+
         } catch (RemoteException e) {
             e.getCause();
         }
@@ -29,16 +36,54 @@ public class Server implements ILeiloes{
 
     @Override
     public void initLeilao(SomeObject objLei, INotification cb) throws RemoteException {
-        cb.sendNotification("CONA!");
+        leiloes.put(objLei.id, new Pair(objLei.getDescription(), objLei.value));
+        cb.sendNotification("Leilao iniciado com sucesso!");
     }
 
     @Override
-    public SomeObject[] getAllLeiloes() throws RemoteException {
-        return new SomeObject[0];
+    public SomeObject[] getAllLeiloes() {
+
+        List<SomeObject> allLeiloes = new LinkedList<>();
+
+        for(String key : leiloes.keySet()) {
+            allLeiloes.add(new SomeObject(key, leiloes.get(key).getDescription(), leiloes.get(key).getValue()));
+        }
+
+        return (SomeObject[]) allLeiloes.toArray();
     }
 
     @Override
     public void licitar(String id, float value, INotification cb) throws RemoteException {
-        cb.sendNotification("CONA!");
+
+        leiloes.get(id).setValue(value);
+        cb.sendNotification("Licitacao guardada com sucesso!");
+    }
+
+    static class Pair {
+
+        private String description;
+        private float value;
+
+        public Pair(String description, Float value) {
+
+            this.setValue(value);
+            this.setDescription(description);
+        }
+
+        public float getValue() {
+            return value;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public void setValue(float value) {
+            this.value = value;
+        }
     }
 }
