92d43f133a650d73138027853eb23fcac101fe6b
Guilherme Arede <gfsarede@gmail.com>
2020-11-07 15:43:41 +0000

    deleted inner class from server

--- AulasPraticas/AP01/Ex2/Server/src/Server.java
+++ AulasPraticas/AP01/Ex2/Server/src/Server.java
@@ -2,18 +2,16 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
-public class Server implements ILeiloes{
+public class Server implements ILeiloes {
 
     private static final String serverIP = "localhost";
     private static final int registerPort = 7000;
     private static final int svcPort = 7001;
 
-    private static HashMap<String, Pair> leiloes;
+    private static HashMap<Integer, SomeObject> leiloes;
+    private static Integer counter = 1;
 
     public static void main(String[] args) {
 
@@ -36,54 +34,35 @@ public class Server implements ILeiloes{
 
     @Override
     public void initLeilao(SomeObject objLei, INotification cb) throws RemoteException {
-        leiloes.put(objLei.id, new Pair(objLei.getDescription(), objLei.value));
+        leiloes.put(counter++, objLei);
         cb.sendNotification("Leilao iniciado com sucesso!");
     }
 
     @Override
     public SomeObject[] getAllLeiloes() {
 
-        List<SomeObject> allLeiloes = new LinkedList<>();
-
-        for(String key : leiloes.keySet()) {
-            allLeiloes.add(new SomeObject(key, leiloes.get(key).getDescription(), leiloes.get(key).getValue()));
-        }
-
+        List<SomeObject> allLeiloes = new LinkedList<>(leiloes.values());
         return allLeiloes.toArray(new SomeObject[0]);
     }
 
     @Override
     public void licitar(String id, float value, INotification cb) throws RemoteException {
 
-        leiloes.get(id).setValue(value);
+        //COMEÇEM A REZAR QUANDO CHEGAR AQUI!
+        leiloes.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().getId().equals(id))
+                .findFirst()
+                .ifPresentOrElse(obj -> obj.getValue().setValue(value), () -> {
+
+                    try {
+                        cb.sendNotification("Leilao com id " + id + " nao encontrado!");
+
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                });
+
         cb.sendNotification("Licitacao guardada com sucesso!");
-    }
-
-    static class Pair {
-
-        private String description;
-        private float value;
-
-        public Pair(String description, Float value) {
-
-            this.setValue(value);
-            this.setDescription(description);
-        }
-
-        public float getValue() {
-            return value;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public void setValue(float value) {
-            this.value = value;
-        }
     }
 }
